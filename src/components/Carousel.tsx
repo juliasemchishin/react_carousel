b9c0271a691d234425b1ceecdbe1773bc89aa843
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2026-01-16 22:35:50 +0200

    added solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+interface CarouselType {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration?: number;
+  infinite?: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<CarouselType> = ({
+  images,
+  step = 3,
+  frameSize = 3,
+  itemWidth = 130,
+  animationDuration = 1000,
+}: CarouselType) => {
+  const [index, setIndex] = useState(0);
+  // console.log('index', index);
+
+  const maxIndex = Math.max(0, images.length - frameSize);
+  const nextIndex = Math.min(maxIndex, index + step);
+  // const prevIndex = Math.max(0, index - step);
+  // console.log('maxIndex', maxIndex);
+  // console.log('nextIndex', nextIndex);
+  // console.log('prevIndex', prevIndex);
+
+  const nextSlide = () => {
+    setIndex(prev => {
+      if (maxIndex === nextIndex) {
+        return prev + 1;
+      }
+
+      return prev + step;
+    });
+  };
+
+  const prevSlide = () => {
+    setIndex(prev => {
+      if (index === 1) {
+        return prev - 1;
+      }
+
+      return prev - step;
+    });
+  };
+
+  return (
+    <div>
+      <div className="Carousel__inputs">
+        <label htmlFor="itemId">Item width</label>
+        <input type="number" value={itemWidth} id="itemId" />
+        <label htmlFor="frameId">Frame size</label>
+        <input type="number" value={frameSize} id="frameId" />
+        <label htmlFor="stepId">Step</label>
+        <input type="number" value={step} id="stepId" />
+      </div>
+
+      <div className="Carousel__wrapper">
+        <button
+          disabled={index === 0}
+          type="button"
+          onClick={prevSlide}
+          className="button"
+        >
+          <img
+            src="/img/right-arrow.png"
+            alt=""
+            className="button__arrow button__arrow__rotate"
+          />
+        </button>
+
+        <div className="Carousel container__list">
+          <ul
+            className="Carousel__list container__list__wrapper"
+            style={{
+              transform: `translateX(-${index * itemWidth}px)`,
+              animationDuration: `${animationDuration}ms`,
+            }}
+          >
+            {images.map((image: string) => (
+              <li key={image}>
+                <img
+                  width={itemWidth}
+                  src={image}
+                  alt=""
+                  className="image carousel__item"
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <button
+          disabled={index === maxIndex}
+          type="button"
+          onClick={nextSlide}
+          data-cy="next"
+          className="button"
+        >
+          <img src="/img/right-arrow.png" alt="" className="button__arrow" />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
